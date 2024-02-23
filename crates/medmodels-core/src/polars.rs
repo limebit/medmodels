d69e233f385b8fc5e093e6ddbfa41e7d150147b1
Jakob Kraus <mail@jakobkraus.de>
2024-02-23 15:28:04 +0100

    test: Write rust medrecord tests

--- crates/medmodels-core/src/polars.rs
+++ crates/medmodels-core/src/polars.rs
@@ -152,3 +152,189 @@ pub(crate) fn dataframe_to_edges(
         })
         .collect()
 }
+
+#[cfg(test)]
+mod test {
+    use std::collections::HashMap;
+
+    use crate::{errors::MedRecordError, polars::dataframe_to_edges};
+
+    use super::{dataframe_to_nodes, MedRecordValue};
+    use polars::prelude::*;
+
+    #[test]
+    fn test_try_from_anyvalue_string() {
+        let any_value = AnyValue::String("value");
+
+        let value = MedRecordValue::try_from(any_value).unwrap();
+
+        assert_eq!(MedRecordValue::String("value".to_string()), value);
+    }
+
+    #[test]
+    fn test_from_anyvalue_int8() {
+        let any_value = AnyValue::Int8(0);
+
+        let value = MedRecordValue::try_from(any_value).unwrap();
+
+        assert_eq!(MedRecordValue::Int(0), value);
+    }
+
+    #[test]
+    fn test_from_anyvalue_int16() {
+        let any_value = AnyValue::Int16(0);
+
+        let value = MedRecordValue::try_from(any_value).unwrap();
+
+        assert_eq!(MedRecordValue::Int(0), value);
+    }
+
+    #[test]
+    fn test_from_anyvalue_int32() {
+        let any_value = AnyValue::Int32(0);
+
+        let value = MedRecordValue::try_from(any_value).unwrap();
+
+        assert_eq!(MedRecordValue::Int(0), value);
+    }
+
+    #[test]
+    fn test_from_anyvalue_int64() {
+        let any_value = AnyValue::Int64(0);
+
+        let value = MedRecordValue::try_from(any_value).unwrap();
+
+        assert_eq!(MedRecordValue::Int(0), value);
+    }
+
+    #[test]
+    fn test_from_anyvalue_float32() {
+        let any_value = AnyValue::Float32(0.0);
+
+        let value = MedRecordValue::try_from(any_value).unwrap();
+
+        assert_eq!(MedRecordValue::Float(0.0), value);
+    }
+
+    #[test]
+    fn test_from_anyvalue_float64() {
+        let any_value = AnyValue::Float64(0.0);
+
+        let value = MedRecordValue::try_from(any_value).unwrap();
+
+        assert_eq!(MedRecordValue::Float(0.0), value);
+    }
+
+    #[test]
+    fn test_from_anyvalue_bool() {
+        let any_value = AnyValue::Boolean(false);
+
+        let value = MedRecordValue::try_from(any_value).unwrap();
+
+        assert_eq!(MedRecordValue::Bool(false), value);
+    }
+
+    #[test]
+    fn test_dataframe_to_nodes() {
+        let s0 = Series::new("index", &["0", "1"]);
+        let s1 = Series::new("attribute", &[1, 2]);
+        let nodes_dataframe = DataFrame::new(vec![s0, s1]).unwrap();
+
+        let nodes = dataframe_to_nodes(nodes_dataframe, "index").unwrap();
+
+        assert_eq!(
+            vec![
+                (
+                    "0".to_string(),
+                    HashMap::from([("attribute".to_string(), 1.into())])
+                ),
+                (
+                    "1".to_string(),
+                    HashMap::from([("attribute".to_string(), 2.into())])
+                )
+            ],
+            nodes
+        );
+    }
+
+    #[test]
+    fn test_invalid_dataframe_to_nodes() {
+        let s0 = Series::new("index", &["0", "1"]);
+        let s1 = Series::new("attribute", &[1, 2]);
+        let nodes_dataframe = DataFrame::new(vec![s0, s1]).unwrap();
+
+        // Providing the wrong index column name should fail
+        assert!(dataframe_to_nodes(nodes_dataframe, "wrong_column")
+            .is_err_and(|e| matches!(e, MedRecordError::ConversionError(_))));
+
+        let s0 = Series::new("index", &[1, 2]);
+        let s1 = Series::new("attribute", &[1, 2]);
+        let nodes_dataframe = DataFrame::new(vec![s0, s1]).unwrap();
+
+        // The index column should be a string column, otherwise fail
+        assert!(dataframe_to_nodes(nodes_dataframe, "index")
+            .is_err_and(|e| matches!(e, MedRecordError::ConversionError(_))));
+    }
+
+    #[test]
+    fn test_dataframe_to_edges() {
+        let s0 = Series::new("from", &["0", "1"]);
+        let s1 = Series::new("to", &["1", "0"]);
+        let s2 = Series::new("attribute", &[1, 2]);
+        let edges_dataframe = DataFrame::new(vec![s0, s1, s2]).unwrap();
+
+        let edges = dataframe_to_edges(edges_dataframe, "from", "to").unwrap();
+
+        assert_eq!(
+            vec![
+                (
+                    "0".to_string(),
+                    "1".to_string(),
+                    HashMap::from([("attribute".to_string(), 1.into())])
+                ),
+                (
+                    "1".to_string(),
+                    "0".to_string(),
+                    HashMap::from([("attribute".to_string(), 2.into())])
+                )
+            ],
+            edges
+        );
+    }
+
+    #[test]
+    fn test_invalid_dataframe_to_edges() {
+        let s0 = Series::new("from", &["0", "1"]);
+        let s1 = Series::new("to", &["1", "0"]);
+        let s2 = Series::new("attribute", &[1, 2]);
+        let edges_dataframe = DataFrame::new(vec![s0, s1, s2]).unwrap();
+
+        // Providing the wrong from index column name should fail
+        assert!(
+            dataframe_to_edges(edges_dataframe.clone(), "wrong_column", "to")
+                .is_err_and(|e| matches!(e, MedRecordError::ConversionError(_)))
+        );
+
+        // Providing the wrong to index column name should fail
+        assert!(dataframe_to_edges(edges_dataframe, "from", "wrong_column")
+            .is_err_and(|e| matches!(e, MedRecordError::ConversionError(_))));
+
+        let s0 = Series::new("from", &[0, 1]);
+        let s1 = Series::new("to", &["1", "0"]);
+        let s2 = Series::new("attribute", &[1, 2]);
+        let edges_dataframe = DataFrame::new(vec![s0, s1, s2]).unwrap();
+
+        // The from index column should be a string column, otherwise fail
+        assert!(dataframe_to_edges(edges_dataframe, "from", "to")
+            .is_err_and(|e| matches!(e, MedRecordError::ConversionError(_))));
+
+        let s0 = Series::new("from", &["0", "1"]);
+        let s1 = Series::new("to", &[1, 0]);
+        let s2 = Series::new("attribute", &[1, 2]);
+        let edges_dataframe = DataFrame::new(vec![s0, s1, s2]).unwrap();
+
+        // The to index column should be a string column, otherwise fail
+        assert!(dataframe_to_edges(edges_dataframe, "from", "to")
+            .is_err_and(|e| matches!(e, MedRecordError::ConversionError(_))));
+    }
+}
