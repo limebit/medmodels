6b0deddc9e73edf84e0a12fb5588ccec2df842a8
Jakob Kraus <mail@jakobkraus.de>
2024-03-21 00:17:30 +0100

    feat: implement missing interface

--- crates/medmodels-core/src/medrecord/graph/node.rs
+++ crates/medmodels-core/src/medrecord/graph/node.rs
@@ -1,20 +1,19 @@
-use std::collections::HashSet;
-
 use super::{Attributes, EdgeIndex};
+use medmodels_utils::aliases::MrHashSet;
 
 #[derive(Debug, Clone)]
 pub struct Node {
     pub attributes: Attributes,
-    pub(super) outgoing_edge_indices: HashSet<EdgeIndex>,
-    pub(super) incoming_edge_indices: HashSet<EdgeIndex>,
+    pub(super) outgoing_edge_indices: MrHashSet<EdgeIndex>,
+    pub(super) incoming_edge_indices: MrHashSet<EdgeIndex>,
 }
 
 impl Node {
     pub fn new(attributes: Attributes) -> Self {
         Self {
             attributes,
-            outgoing_edge_indices: HashSet::new(),
-            incoming_edge_indices: HashSet::new(),
+            outgoing_edge_indices: MrHashSet::new(),
+            incoming_edge_indices: MrHashSet::new(),
         }
     }
 }
--- crates/medmodels-utils/src/aliases.rs
+++ crates/medmodels-utils/src/aliases.rs
@@ -1,1 +1,3 @@
 pub type MrHashMap<K, V> = hashbrown::HashMap<K, V>;
+pub type MrHashMapEntry<'a, K, V, S> = hashbrown::hash_map::Entry<'a, K, V, S>;
+pub type MrHashSet<T> = hashbrown::HashSet<T>;
