7c2914a996e5a49c8452c7abb7805e79265f439c
MarIniOnz <martin.iniguez@limebit.de>
2024-05-27 14:58:31 +0200

    test: Refactoring tests for Mahalanobis metric

--- medmodels/matching/metrics.py
+++ medmodels/matching/metrics.py
@@ -14,17 +14,17 @@ def absolute_metric(
     of the absolute difference between them. This distance is the sum of the absolute
     differences between each corresponding pair of elements in the two vectors.
 
+    The calculation is based on the formula:
+    $$
+    D(x, y) = ||x - y||_1 = \\sum_{i=1}^n |x_i - y_i| for x, y \\in \\mathbb{R}^n
+    $$
+
     Args:
         vector1 (NDArray[np.float64]): The first vector to be compared.
         vector2 (NDArray[np.float64]): The second vector to be compared.
 
     Returns:
         float: The Manhattan distance between the two vectors.
-
-    The calculation is based on the formula:
-        $$
-        D(x, y) = ||x - y||_1 = \\sum_{i=1}^n |x_i - y_i| for x, y \\in \\mathbb{R}^n
-        $$
     """
 
     diff = vector1 - vector2
@@ -59,47 +59,54 @@ def exact_metric(vector1: NDArray[np.float64], vector2: NDArray[np.float64]) -> 
     Note:
         This function is designed for exactly two input vectors.
     """
+
     if np.array_equal(vector1, vector2):
         return 0
 
     return np.inf
 
 
-def mahalanobis_metric(*vectors: NDArray[np.float64], inv_cov: NDArray[np.float64]):
+def mahalanobis_metric(
+    vector1: NDArray[np.float64],
+    vector2: NDArray[np.float64],
+    inv_cov: NDArray[np.float64],
+) -> float:
     """
     Returns mahalanobis metric for matching. Works better with continuous covariates.
 
-    .. math::
-        D(x, y) = \\sqrt{(x-y)^T S^{-1} (x-y)} where S is the covariate matrix of
-        the whole distribution
+    $$
+    D(x, y) = \\sqrt{(x-y)^T S^{-1} (x-y)} \\\\
+    \\text{where } S \\text{ is the covariance matrix of the whole distribution}
+    $$
 
-    Args:
-        vectors (NDArray[np.float64]): Two numpy arrays to be compared. They must
-            belong to the same distribution.
-        inv_cov (NDArray[np.float64]): The inverse of the covariance matrix of the
-            whole distribution (data set).
-
-    Returns:
-        float: The Mahalanobis distance between the two vectors.
-
-    The covariance matrix and its inverse are calculated at most ones per item to be
+    The covariance matrix and its inverse are calculated at most once per item to be
     paired, hence, they won't be included inside of the method in order to avoid the
     repeated computation.
 
     By matching without replacement the found paired item will be removed from the set,
-    hence,the covariance matrix of the whole distribution and its inverse need to be
+    hence, the covariance matrix of the whole distribution and its inverse need to be
     recalculated for every entry. This can be time consuming for big data sets (esp.
     with a big amount of features).
+
+    Args:
+        vector1 (NDArray[np.float64]): The first vector to be compared.
+        vector2 (NDArray[np.float64]): The second vector to be compared. Must have the
+            same shape as `vector1` and belong to the same distribution.
+        inv_cov (NDArray[np.float64]): The inverse of the covariance matrix of the whole
+            distribution (data set).
+
+    Returns:
+        float: The Mahalanobis distance between the two vectors.
     """
 
-    diff = vectors[0] - vectors[1]
-    left = np.dot(diff, inv_cov)
-    right = np.dot(left, diff)
+    diff = vector1 - vector2
 
-    if isinstance(right, np.ndarray):
-        right = right.item()
+    if inv_cov.ndim == 0:
+        distance_squared = (diff**2 * inv_cov).item()
+    else:
+        distance_squared = diff.T @ inv_cov @ diff
 
-    return float(math.sqrt(right))
+    return float(math.sqrt(distance_squared))
 
 
 METRICS = {
