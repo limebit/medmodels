d9eae729833c041d51f2477e289276e94a9f9f63
MarIniOnz <martin.iniguez@limebit.de>
2024-06-17 13:58:58 +0200

    feat: Pandas to polars, change of variable names and adding +1 neighbors.

--- medmodels/matching/algorithms/propensity_score.py
+++ medmodels/matching/algorithms/propensity_score.py
@@ -1,15 +1,28 @@
 from __future__ import annotations
 
-from typing import Any, Dict, List, Optional, Tuple, Union
+from typing import TYPE_CHECKING, Any, Dict, Literal, Optional, Tuple, Union
 
 import numpy as np
-import pandas as pd
+import polars as pl
 from numpy.typing import NDArray
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
 
 from medmodels.matching.algorithms.classic_distance_models import nearest_neighbor
+from medmodels.matching.metrics import Metric
+from medmodels.medrecord.types import MedRecordAttributeInputList
+
+if TYPE_CHECKING:
+    import sys
+
+    if sys.version_info >= (3, 10):
+        from typing import TypeAlias
+    else:
+        from typing_extensions import TypeAlias
+
+
+Model: TypeAlias = Literal["logit", "dec_tree", "forest"]
 
 
 def calculate_propensity(
@@ -17,8 +30,8 @@ def calculate_propensity(
     y_train: NDArray[Union[np.int64, np.float64]],
     treated_test: NDArray[Union[np.int64, np.float64]],
     control_test: NDArray[Union[np.int64, np.float64]],
+    model: Model = "logit",
     hyperparam: Optional[Dict[str, Any]] = None,
-    metric: str = "logit",
 ) -> Tuple[NDArray[np.float64], NDArray[np.float64]]:
     """
     Trains a classification algorithm on training data, predicts the probability of
@@ -36,21 +49,21 @@ def calculate_propensity(
             treated group to predict probabilities.
         control_test (NDArray[Union[np.int64, np.float64]]): Feature matrix for the
             control group to predict probabilities.
+        model (Model, optional): Classification algorithm to use. Options: "logit",
+            "dec_tree", "forest".
         hyperparam (Optional[Dict[str, Any]], optional): Manual hyperparameter settings.
             Uses default if None.
-        metric (str, optional): Classification algorithm to use. Options: "logit",
-            "dec_tree", "forest".
 
     Returns:
         Tuple[NDArray[np.float64], NDArray[np.float64]: Probabilities of the positive
             class for treated and control groups.
 
     Example:
-        For "dec_tree" metric with iris dataset inputs, returns probabilities of the
+        For "dec_tree" model with iris dataset inputs, returns probabilities of the
         last class for treated and control sets, e.g., ([0.], [0.]).
     """
 
-    propensity_metric = PROP_MODEL[metric]
+    propensity_metric = PROP_MODEL[model]
 
     if hyperparam:
         pm = propensity_metric(**hyperparam)
@@ -68,41 +81,46 @@ def calculate_propensity(
 
 
 def run_propensity_score(
-    treated_set: pd.DataFrame,
-    control_set: pd.DataFrame,
-    model: str = "logit",
-    hyperparam: Optional[Any] = None,
-    covariates: Optional[Union[List[str], pd.Index[str]]] = None,
-) -> pd.DataFrame:
+    treated_set: pl.DataFrame,
+    control_set: pl.DataFrame,
+    model: Model = "logit",
+    metric: Metric = "absolute",
+    number_of_neighbors: int = 1,
+    hyperparam: Optional[Dict[str, Any]] = None,
+    covariates: Optional[MedRecordAttributeInputList] = None,
+) -> pl.DataFrame:
     """
     Executes Propensity Score matching using a specified classification algorithm.
     Constructs the training target by assigning 1 to the treated set and 0 to the
     control set, then predicts the propensity score. This score is used for matching
     using the nearest neighbor method.
 
+    This function simplifies the process of propensity score matching, focusing on the
+    use of the propensity score as the sole covariate for matching.
+
     Args:
-        treated_set (pd.DataFrame): Data for the treated group.
-        control_set (pd.DataFrame): Data for the control group.
-        model (str, optional): Classification algorithm for predicting probabilities.
+        treated_set (pl.DataFrame): Data for the treated group.
+        control_set (pl.DataFrame): Data for the control group.
+        model (Model, optional): Classification algorithm for predicting probabilities.
             Options include "logit", "dec_tree", "forest".
-        hyperparam (Optional[Any], optional): Hyperparameters for model tuning.
-            Increases computation time if set.
-        covariates (Optional[Union[List[str], pd.Index[str]]], optional): Features for
+        metric (Metric, optional): Metric for matching. Options include "absolute",
+            "mahalanobis", "exact". Defaults to "absolute".
+        number_of_neighbors (int, optional): Number of nearest neighbors to find for
+            each treated unit. Defaults to 1.
+        hyperparam (Optional[Dict[str, Any]], optional): Hyperparameters for model
+            tuning. Increases computation time if set. Uses default if None.
+        covariates (Optional[MedRecordAttributeInputList], optional): Features for
             matching. Uses all if None.
 
     Returns:
-        pd.DataFrame: Matched subset from the control set corresponding to the treated
+        pl.DataFrame: Matched subset from the control set corresponding to the treated
             set.
-
-    This function simplifies the process of propensity score matching, focusing on the
-    use of the propensity score as the sole covariate for matching.
     """
-
     if not covariates:
         covariates = treated_set.columns
 
-    treated_array = treated_set[covariates].to_numpy().astype(float)
-    control_array = control_set[covariates].to_numpy().astype(float)
+    treated_array = treated_set.select(covariates).to_numpy().astype(float)
+    control_array = control_set.select(covariates).to_numpy().astype(float)
 
     x_train = np.concatenate((treated_array, control_array))
     len_treated, len_control = len(treated_array), len(control_array)
@@ -114,20 +132,24 @@ def run_propensity_score(
         treated_array,
         control_array,
         hyperparam=hyperparam,
-        metric=model,
+        model=model,
     )
 
     # Add propensity score to the original data
-    treated_set["Prop. score"] = treated_prop
-    control_set["Prop. score"] = control_prop
+    treated_set = treated_set.with_columns(pl.Series("prop_score", treated_prop))
+    control_set = control_set.with_columns(pl.Series("prop_score", control_prop))
 
     matched_control = nearest_neighbor(
-        treated_set, control_set, metric="absolute", covariates=["Prop. score"]
+        treated_set,
+        control_set,
+        metric=metric,
+        number_of_neighbors=number_of_neighbors,
+        covariates=["prop_score"],
     )
 
-    matched_control.pop("Prop. score")
-    treated_set.pop("Prop. score")
-    control_set.pop("Prop. score")
+    matched_control = matched_control.drop("prop_score")
+    treated_set = treated_set.drop("prop_score")
+    control_set = control_set.drop("prop_score")
 
     return matched_control
 
