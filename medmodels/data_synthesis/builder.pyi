8aa3d73ecd4b5b8e7c36a03b1a738241169e35e4
MarIniOnz <mioiniguez@gmail.com>
2024-08-21 11:59:53 +0200

    feat: renaming back to .pyi and keeping .pyi files till they can be erased

---
+++ medmodels/data_synthesis/builder.pyi
@@ -0,0 +1,8 @@
+"""Builder for data Synthesizers."""
+
+from __future__ import annotations
+
+from medmodels.data_synthesis.mtgan.builder import MTGANBuilder
+
+class SynthesizerBuilder:
+    def from_mtgan(self) -> MTGANBuilder: ...
---
+++ medmodels/data_synthesis/synthesizer.pyi
@@ -0,0 +1,45 @@
+"""Abstract class for synthesizers."""
+
+from __future__ import annotations
+
+from pathlib import Path
+from typing import TYPE_CHECKING, Dict, Optional
+
+import torch
+from torch import nn
+
+from medmodels.data_synthesis.builder import SynthesizerBuilder
+from medmodels.medrecord.medrecord import MedRecord
+
+if TYPE_CHECKING:
+    from medmodels.data_synthesis.synthesizer_model import SynthesizerModel
+
+class Synthesizer(nn.Module):
+    """Synthesizer is an abstract class that serves as a blueprint for synthesizers.
+    It ensures the correct instantiation, training, and persistence of
+    models.
+    """
+
+    preprocessor: nn.Module
+    postprocessor: nn.Module
+    device: torch.device
+
+    def __init__(
+        self,
+        preprocessor: nn.Module,
+        postprocessor: nn.Module,
+    ) -> None: ...
+    @classmethod
+    def builder(cls) -> SynthesizerBuilder: ...
+    def preprocess(self, medrecord: MedRecord) -> MedRecord: ...
+    def fit(
+        self, medrecord: MedRecord, save_directory: Optional[Path]
+    ) -> SynthesizerModel: ...
+    def fit_from(
+        self,
+        medrecord: MedRecord,
+        save_directory: Optional[Path],
+        **kwargs: Dict[str, Path],
+    ) -> SynthesizerModel: ...
+    def save_model(self, path: Path) -> None: ...
+    def load_model(self, path: Path) -> SynthesizerModel: ...
