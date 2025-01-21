2e4ee977a2078c2d4b5e70b8a0083fd2f15a5b62
Martin Iniguez <60970451+MarIniOnz@users.noreply.github.com>
2025-01-21 15:47:44 +0100

    fix: the way essential covariates and one-hot-encoded ones are processed (#256)

--- medmodels/treatment_effect/matching/propensity.py
+++ medmodels/treatment_effect/matching/propensity.py
@@ -2,11 +2,12 @@
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Any, Dict, Optional, Set
+from typing import TYPE_CHECKING, Any, Dict, Optional, Sequence, Set
 
 import numpy as np
 import polars as pl
 
+from medmodels import MedRecord
 from medmodels.treatment_effect.matching.algorithms.classic_distance_models import (
     nearest_neighbor,
 )
@@ -18,7 +19,7 @@ from medmodels.treatment_effect.matching.matching import Matching
 
 if TYPE_CHECKING:
     from medmodels import MedRecord
-    from medmodels.medrecord.types import MedRecordAttributeInputList, NodeIndex
+    from medmodels.medrecord.types import Group, MedRecordAttribute, NodeIndex
 
 
 class PropensityMatching(Matching):
@@ -40,23 +41,22 @@ class PropensityMatching(Matching):
         *,
         model: Model = "logit",
         number_of_neighbors: int = 1,
-        hyperparam: Optional[Dict[str, Any]] = None,
+        hyperparameters: Optional[Dict[str, Any]] = None,
     ) -> None:
         """Initializes the propensity score class.
 
         Args:
             model (Model, optional): classification method to be used, default: "logit".
                 Can be chosen from ["logit", "dec_tree", "forest"].
-            nearest_neighbors_algorithm (NNAlgorithm, optional): algorithm used to
-                compute nearest neighbors. Defaults to "auto".
             number_of_neighbors (int, optional): number of neighbors to be matched per
                 treated subject. Defaults to 1.
-            hyperparam (Optional[Dict[str, Any]], optional): hyperparameters for the
-                classification model, default: None.
+            hyperparameters (Optional[Dict[str, Any]], optional): hyperparameters for
+                the classification model. Defaults to None.
         """
+        super().__init__(number_of_neighbors)
         self.model = model
         self.number_of_neighbors = number_of_neighbors
-        self.hyperparam = hyperparam
+        self.hyperparameters = hyperparameters
 
     def match_controls(
         self,
@@ -64,37 +64,37 @@ class PropensityMatching(Matching):
         medrecord: MedRecord,
         control_set: Set[NodeIndex],
         treated_set: Set[NodeIndex],
-        essential_covariates: Optional[MedRecordAttributeInputList] = None,
-        one_hot_covariates: Optional[MedRecordAttributeInputList] = None,
+        patients_group: Group,
+        essential_covariates: Optional[Sequence[MedRecordAttribute]] = None,
+        one_hot_covariates: Optional[Sequence[MedRecordAttribute]] = None,
     ) -> Set[NodeIndex]:
         """Matches the controls based on propensity score matching.
 
         Args:
             medrecord (MedRecord): medrecord object containing the data.
-            treated_set (Set[NodeIndex]): Set of treated subjects.
             control_set (Set[NodeIndex]): Set of control subjects.
-            essential_covariates (Optional[MedRecordAttributeInputList], optional):
-                Covariates that are essential for matching. Defaults to
-                ["gender", "age"].
-            one_hot_covariates (Optional[MedRecordAttributeInputList], optional):
-                Covariates that are one-hot encoded for matching. Defaults to
-                ["gender"].
+            treated_set (Set[NodeIndex]): Set of treated subjects.
+            patients_group (Group): Group of patients in MedRecord.
+            essential_covariates (Optional[Sequence[MedRecordAttribute]], optional):
+                Covariates that are essential for matching. Defaults to None, meaning
+                all the attributes of the patients are used.
+            one_hot_covariates (Optional[Sequence[MedRecordAttribute]], optional):
+                Covariates that are one-hot encoded for matching. Defaults to None,
+                meaning all the categorical attributes of the patients are used.
 
         Returns:
             Set[NodeIndex]:  Node Ids of the matched controls.
         """
-        if essential_covariates is None:
-            essential_covariates = ["gender", "age"]
-        if one_hot_covariates is None:
-            one_hot_covariates = ["gender"]
-
         # Preprocess the data
         data_treated, data_control = self._preprocess_data(
             medrecord=medrecord,
             treated_set=treated_set,
             control_set=control_set,
-            essential_covariates=essential_covariates,
-            one_hot_covariates=one_hot_covariates,
+            patients_group=patients_group,
+            essential_covariates=list(essential_covariates)
+            if essential_covariates
+            else None,
+            one_hot_covariates=list(one_hot_covariates) if one_hot_covariates else None,
         )
 
         # Convert the Polars DataFrames to NumPy arrays
@@ -116,7 +116,7 @@ class PropensityMatching(Matching):
             y_train=y_train,
             treated_test=treated_array,
             control_test=control_array,
-            hyperparam=self.hyperparam,
+            hyperparameters=self.hyperparameters,
             model=self.model,
         )
 
