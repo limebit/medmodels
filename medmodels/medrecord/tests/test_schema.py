b648c21eb9cb6ad558ac3a61649eb137d43282b3
Jakob Kraus <52459467+JabobKrauskopf@users.noreply.github.com>
2025-02-12 21:22:57 +0100

    feat: implement new functions

--- medmodels/medrecord/tests/test_schema.py
+++ medmodels/medrecord/tests/test_schema.py
@@ -1,208 +1,106 @@
 import unittest
 
-import pytest
-
 import medmodels.medrecord as mr
 from medmodels._medmodels import PyAttributeType
-from medmodels.medrecord.schema import GroupSchema, Schema
 
 
 def create_medrecord() -> mr.MedRecord:
     return mr.MedRecord.from_simple_example_dataset()
 
 
-class TestSchema(unittest.TestCase):
-    def setUp(self) -> None:
-        self.schema = create_medrecord().schema
+class TestAttributeType(unittest.TestCase):
+    def test_from_py_attribute_type(self) -> None:
+        assert (
+            mr.AttributeType._from_py_attribute_type(PyAttributeType.Categorical)
+            == mr.AttributeType.Categorical
+        )
+        assert (
+            mr.AttributeType._from_py_attribute_type(PyAttributeType.Continuous)
+            == mr.AttributeType.Continuous
+        )
+        assert (
+            mr.AttributeType._from_py_attribute_type(PyAttributeType.Temporal)
+            == mr.AttributeType.Temporal
+        )
+        assert (
+            mr.AttributeType._from_py_attribute_type(PyAttributeType.Unstructured)
+            == mr.AttributeType.Unstructured
+        )
 
-    def test_groups(self) -> None:
-        assert sorted(
-            [
-                "diagnosis",
-                "drug",
-                "patient_diagnosis",
-                "patient_drug",
-                "patient_procedure",
-                "patient",
-                "procedure",
-            ]
-        ) == sorted(self.schema.groups)
-
-    def test_group(self) -> None:
-        assert isinstance(self.schema.group("patient"), mr.GroupSchema)  # pyright: ignore[reportUnnecessaryIsInstance]
-
-        with pytest.raises(ValueError, match=r"Group nonexistent not found in schema."):
-            self.schema.group("nonexistent")
-
-
-class TestGroupSchema(unittest.TestCase):
-    def setUp(self) -> None:
-        self.schema = create_medrecord().schema
-
-    def test_nodes(self) -> None:
-        assert self.schema.group("patient").nodes == {
-            "age": (mr.Int(), mr.AttributeType.Continuous),
-            "gender": (mr.String(), mr.AttributeType.Categorical),
-        }
-
-    def test_edges(self) -> None:
-        assert self.schema.group("patient_diagnosis").edges == {
-            "time": (mr.DateTime(), mr.AttributeType.Temporal),
-            "duration_days": (mr.Option(mr.Float()), mr.AttributeType.Continuous),
-        }
-
-
-class TestAttributesSchema(unittest.TestCase):
-    def setUp(self) -> None:
-        self.attributes_schema = (
-            Schema(
-                groups={"diagnosis": GroupSchema(nodes={"description": mr.String()})},
-            )
-            .group("diagnosis")
-            .nodes
+    def test_into_py_attribute_type(self) -> None:
+        assert (
+            mr.AttributeType.Categorical._into_py_attribute_type()
+            == PyAttributeType.Categorical
+        )
+        assert (
+            mr.AttributeType.Continuous._into_py_attribute_type()
+            == PyAttributeType.Continuous
+        )
+        assert (
+            mr.AttributeType.Temporal._into_py_attribute_type()
+            == PyAttributeType.Temporal
+        )
+        assert (
+            mr.AttributeType.Unstructured._into_py_attribute_type()
+            == PyAttributeType.Unstructured
         )
 
     def test_repr(self) -> None:
-        assert (
-            repr(self.attributes_schema)
-            == "{'description': (DataType.String, AttributeType.Unstructured)}"
-        )
+        assert repr(mr.AttributeType.Categorical) == "AttributeType.Categorical"
+        assert repr(mr.AttributeType.Continuous) == "AttributeType.Continuous"
+        assert repr(mr.AttributeType.Temporal) == "AttributeType.Temporal"
+        assert repr(mr.AttributeType.Unstructured) == "AttributeType.Unstructured"
 
-        second_attributes_schema = (
-            Schema(
-                groups={
-                    "diagnosis": GroupSchema(
-                        nodes={
-                            "description": (mr.String(), mr.AttributeType.Categorical)
-                        }
-                    )
-                },
-            )
-            .group("diagnosis")
-            .nodes
-        )
-
-        assert (
-            repr(second_attributes_schema)
-            == "{'description': (DataType.String, AttributeType.Categorical)}"
-        )
-
-    def test_getitem(self) -> None:
-        assert (mr.String(), mr.AttributeType.Unstructured) == self.attributes_schema[
-            "description"
-        ]
-
-        with pytest.raises(KeyError):
-            self.attributes_schema["nonexistent"]
-
-    def test_contains(self) -> None:
-        assert "description" in self.attributes_schema
-        assert "nonexistent" not in self.attributes_schema
-
-    def test_len(self) -> None:
-        assert len(self.attributes_schema) == 1
-
-    def test_eq(self) -> None:
-        comparison_attributes_schema = (
-            Schema(
-                groups={"diagnosis": GroupSchema(nodes={"description": mr.String()})},
-            )
-            .group("diagnosis")
-            .nodes
-        )
-
-        assert self.attributes_schema == comparison_attributes_schema
-
-        comparison_attributes_schema = (
-            Schema(
-                groups={"diagnosis": GroupSchema(nodes={"description": mr.Int()})},
-            )
-            .group("diagnosis")
-            .nodes
-        )
-
-        assert self.attributes_schema != comparison_attributes_schema
-
-        comparison_attributes_schema = (
-            Schema(
-                groups={
-                    "diagnosis": GroupSchema(
-                        nodes={
-                            "description": (mr.String(), mr.AttributeType.Categorical)
-                        }
-                    )
-                },
-            )
-            .group("diagnosis")
-            .nodes
-        )
-
-        assert self.attributes_schema != comparison_attributes_schema
-
-        comparison_attributes_schema = (
-            Schema(
-                groups={
-                    "diagnosis": GroupSchema(
-                        nodes={
-                            "description2": (mr.String(), mr.AttributeType.Categorical)
-                        }
-                    )
-                },
-            )
-            .group("diagnosis")
-            .nodes
-        )
-
-        assert self.attributes_schema != comparison_attributes_schema
-
-        assert self.attributes_schema is not None
-
-    def test_keys(self) -> None:
-        assert list(self.attributes_schema.keys()) == ["description"]
-
-    def test_values(self) -> None:
-        assert [(mr.String(), mr.AttributeType.Unstructured)] == list(
-            self.attributes_schema.values()
-        )
-
-    def test_items(self) -> None:
-        assert [("description", (mr.String(), mr.AttributeType.Unstructured))] == list(
-            self.attributes_schema.items()
-        )
-
-
-class TestAttributeType(unittest.TestCase):
     def test_str(self) -> None:
         assert str(mr.AttributeType.Categorical) == "Categorical"
         assert str(mr.AttributeType.Continuous) == "Continuous"
         assert str(mr.AttributeType.Temporal) == "Temporal"
+        assert str(mr.AttributeType.Unstructured) == "Unstructured"
+
+    def test_hash(self) -> None:
+        assert hash(mr.AttributeType.Categorical) == hash("Categorical")
+        assert hash(mr.AttributeType.Continuous) == hash("Continuous")
+        assert hash(mr.AttributeType.Temporal) == hash("Temporal")
+        assert hash(mr.AttributeType.Unstructured) == hash("Unstructured")
 
     def test_eq(self) -> None:
         assert mr.AttributeType.Categorical == mr.AttributeType.Categorical
         assert mr.AttributeType.Categorical == PyAttributeType.Categorical
         assert mr.AttributeType.Categorical != mr.AttributeType.Continuous
+        assert mr.AttributeType.Categorical != mr.AttributeType.Temporal
+        assert mr.AttributeType.Categorical != mr.AttributeType.Unstructured
         assert mr.AttributeType.Categorical != PyAttributeType.Continuous
+        assert mr.AttributeType.Categorical != PyAttributeType.Temporal
+        assert mr.AttributeType.Categorical != PyAttributeType.Unstructured
 
         assert mr.AttributeType.Continuous == mr.AttributeType.Continuous
         assert mr.AttributeType.Continuous == PyAttributeType.Continuous
         assert mr.AttributeType.Continuous != mr.AttributeType.Categorical
+        assert mr.AttributeType.Continuous != mr.AttributeType.Temporal
+        assert mr.AttributeType.Continuous != mr.AttributeType.Unstructured
         assert mr.AttributeType.Continuous != PyAttributeType.Categorical
+        assert mr.AttributeType.Continuous != PyAttributeType.Temporal
+        assert mr.AttributeType.Continuous != PyAttributeType.Unstructured
 
         assert mr.AttributeType.Temporal == mr.AttributeType.Temporal
         assert mr.AttributeType.Temporal == PyAttributeType.Temporal
         assert mr.AttributeType.Temporal != mr.AttributeType.Categorical
+        assert mr.AttributeType.Temporal != mr.AttributeType.Continuous
+        assert mr.AttributeType.Temporal != mr.AttributeType.Unstructured
         assert mr.AttributeType.Temporal != PyAttributeType.Categorical
+        assert mr.AttributeType.Temporal != PyAttributeType.Continuous
+        assert mr.AttributeType.Temporal != PyAttributeType.Unstructured
+
+        assert mr.AttributeType.Unstructured == mr.AttributeType.Unstructured
+        assert mr.AttributeType.Unstructured == PyAttributeType.Unstructured
+        assert mr.AttributeType.Unstructured != mr.AttributeType.Categorical
+        assert mr.AttributeType.Unstructured != mr.AttributeType.Continuous
+        assert mr.AttributeType.Unstructured != mr.AttributeType.Temporal
+        assert mr.AttributeType.Unstructured != PyAttributeType.Categorical
+        assert mr.AttributeType.Unstructured != PyAttributeType.Continuous
+        assert mr.AttributeType.Unstructured != PyAttributeType.Temporal
 
 
 if __name__ == "__main__":
-    run_test = unittest.TestLoader().loadTestsFromTestCase(TestSchema)
-    unittest.TextTestRunner(verbosity=2).run(run_test)
-
-    run_test = unittest.TestLoader().loadTestsFromTestCase(TestGroupSchema)
-    unittest.TextTestRunner(verbosity=2).run(run_test)
-
-    run_test = unittest.TestLoader().loadTestsFromTestCase(TestAttributesSchema)
-    unittest.TextTestRunner(verbosity=2).run(run_test)
-
     run_test = unittest.TestLoader().loadTestsFromTestCase(TestAttributeType)
     unittest.TextTestRunner(verbosity=2).run(run_test)
